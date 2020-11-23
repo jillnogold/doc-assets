8208513742553cbae5af8f337d90b276dd1bf766
Sharon Lifshitz <sharonl@iguazio.com>
2020-11-23 22:14:39 +0200

    Root public-site .htaccess: reidrect obsolete doc-drafts/vm-install URLs to latest-release/
    MERGE `develppment` PR #26 & PR #27

--- htaccess/.htaccess.public.root.sh
+++ htaccess/.htaccess.public.root.sh
@@ -134,6 +134,19 @@ RewriteRule ^.*$ $0 [R=302,L,NE]
 # handled from docs/.htaccess using the new URLs from the following redirect.
 RedirectMatch 301 ^/(docs-archive|techpreview)(|/.*)$ https://www.iguazio.com/docs/$2
 
+## Obsolete Temporary Draft-Sites Redirects
+## doc-drafts/vm-install/ - V2.10.0 latest-release VM-installation draft site
+# [InfInfo] (sharonl) (23.11.20) The draft site was replaced with public on-prem
+# VM installation docs (doc-site PR #2226). NOTE: The draft site had a different
+# docs tree for VM installation docs. I didn't want to add specific redirect
+# rules for each page, so I redirect any page in the VM installation-docs
+# section to the root page of this section, and then the remaining draft-site
+# pages are redirected to the respective pages on the latest-release doc site.
+# TODO: Remove the redirect rule after an adjustment period for
+# customers who had used the draft site.
+RedirectMatch 301 ^/doc-drafts/vm-install(/intro/setup/on-prem/vm)(|/.*)$ https://www.iguazio.com/docs/latest-release$1/
+RedirectMatch 301 ^/doc-drafts/vm-install(|/.*)$ https://www.iguazio.com/docs/latest-release/$1
+
 ## Docs home (base doc-site URL) > latest-release/ home
 RedirectMatch 301 ^/(docs)(/|)$ https://www.iguazio.com/$1/latest-release/
 
